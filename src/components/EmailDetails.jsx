72d90c592d6116e53e55783363ceb2256aa17eef
Nikolai L. Børseth <nikolailb1999@gmail.com>
2025-02-05 11:36:15 +0100

    Stole some styling for email details from https://github.com/boolean-uk/react-gmail-intro/tree/main

--- src/components/EmailDetails.jsx
+++ src/components/EmailDetails.jsx
@@ -1,13 +1,59 @@
-import "../styles/Email.css";
+import "../styles/EmailDetails.css";
+import backArrow from "../assets/icons/back-arrow.png";
+import returnArrow from "../assets/icons/return-arrow.png";
+import rateStarButton from "../assets/icons/rate-star-button.png";
+import rubbishButton from "../assets/icons/rubbish-bin-delete-button.png";
 
 function EmailDetails({ email, onBack }) {
   return (
-    <div className="email-details">
-      <button onClick={onBack}>Back</button>
-      <h2>{email.title}</h2>
-      <p><strong>From:</strong> {email.sender}</p>
-      <p>{email.body}</p>
-    </div>
+    <article className="email-content">
+      <div className="email-actions">
+        <img className="icon" src={returnArrow} alt="reply button" onClick={onBack} width={32}/>
+      </div>
+      <div className="title">
+        <h1>{email.title}</h1>
+      </div>
+      <header className="email-content--header">
+        <div className="avatar"></div>
+        <div className="email-info">
+          <div className="sender-info">
+            <h2>{email.sender}</h2>
+            <em>&lt;no-reply@example.com&gt;</em>
+          </div>
+          <div className="user-info">
+            <p>
+              to me <em>&lt;nicolas@boolean.co.uk&gt;</em>
+            </p>
+          </div>
+        </div>
+        <div className="date-info">
+          <p>17 March 2021, 09:33</p>
+        </div>
+        <div className="email-action-icons">
+          <ul>
+            <li>
+              <img className="icon" src={backArrow} alt="reply button" onClick={onBack} />
+            </li>
+            <li>
+              <img className="icon" src={rateStarButton} alt="star button" />
+            </li>
+            <li>
+              <img className="icon" src={rubbishButton} alt="delete button" />
+            </li>
+          </ul>
+        </div>
+      </header>
+      <hr className="seperator"/>
+      <section className="email-body">
+        <div className="content">
+          <p>{email.body}</p>
+        </div>
+      </section>
+      <section className="email-actions">
+        <button>Reply</button>
+        <button>Forward</button>
+      </section>
+    </article>
   );
 }
 
